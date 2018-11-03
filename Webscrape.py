bd7660ef4b633f0341bdcd2703ee12d08e105c7d
spark638 <spark638@gatech.edu>
2018-11-03 19:11:31 -0400

    can save csv file

--- Webscrape.py
+++ Webscrape.py
@@ -7,7 +7,7 @@ import csv
 
 # browser = webdriver.Firefox()
 options = Options()
-options.set_headless(headless=False)
+options.set_headless(headless=True)
 browser = webdriver.Firefox(firefox_options=options, executable_path=r'C:/Users/com93/Desktop/geckodriver.exe')
 url = 'http://stats.ncaa.org/teams/312390'
 
@@ -73,16 +73,24 @@ try:
                 if e.get_attribute("class") != "grey_heading":
                     tdText = e.find_element_by_xpath(".//td[" + str(num) + "]").text
                     if tdText != "":
-                        print(tdText)
+                        # print(tdText)
+                        with open('playbyplay.csv', 'a', newline = '') as outfile:
+                            w = csv.writer(outfile)
+                            w.writerow({tdText.replace(",", " ")}) # need to replace commas with spaces because it's csv
 
         browser.close() # close new window
         browser.switch_to_window(opp_win) # switch back to main window
 
-    # Get opponent's team stats
-    element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[2]/table[2]/tbody/tr[13]/td/a")
-    element.click()
-    sleep(5)
+    # # Get opponent's team stats
+    # element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[2]/table[2]/tbody/tr[13]/td/a")
+    # element.click()
+    # sleep(5)
 
+    # element = browser.find_elements_by_xpath("//*[@id=\"stat_grid\"]/tbody/tr")
+    # for row in element:
+    #     column = row.find_elements_by_xpath(".//td")
+    #     for c in column:
+    #         print(c.text)
 
 except Exception as e:
     print(e)
