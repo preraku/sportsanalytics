fc584aea9091e55f10b9f386cf419b950d0a9c4f
spark638 <spark638@gatech.edu>
2018-11-03 16:06:51 -0400

    can open last 15 results

--- Webscrape.py
+++ Webscrape.py
@@ -23,14 +23,11 @@ try:
         elementNum += 1
         element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[1]/table/tbody/tr[" + str(elementNum) + "]/td[1]")
         currDate = element.text
-        # print(currDate)
 
-    # Open a new browser for opponent team
-    cur_win = browser.current_window_handle # Georgia Tech window
+    # Open the opponent team's webpage
     element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[1]/table/tbody/tr[" + str(elementNum) + "]/td[2]/a")
-    element.send_keys(Keys.SHIFT + Keys.RETURN)
+    element.click()
     sleep(15)
-    browser.switch_to_window([win for win in browser.window_handles if win !=cur_win][0]) # switch to new window
 
     # Find past 15 games played by the opponent
     date = "05/10/2018"
@@ -42,127 +39,18 @@ try:
         element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[1]/table/tbody/tr[" + str(elementNum) + "]/td[1]")
         currDate = element.text
 
-    # Get last 15 games
+    # Get last 15 games window
     for game in range(elementNum - 15, elementNum):
         element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[1]/table/tbody/tr[" + str(game) + "]/td[1]")
         currDate = element.text
+        opp_win = browser.current_window_handle # Opponent window
+        element = browser.find_element_by_xpath("//*[@id=\"contentarea\"]/table/tbody/tr/td[1]/table/tbody/tr[" + str(game) + "]/td[3]/a")
+        element.send_keys(Keys.SHIFT + Keys.RETURN)
+        sleep(5)
+        browser.switch_to_window([win for win in browser.window_handles if win !=opp_win][0]) # switch to new window
 
-
-
-    # numMatch = 0
-
-    # for year in range(1, 3):
-    #     element = browser.find_element_by_xpath("//*[@id=\"date-config\"]")
-    #     # element.click()
-    #     browser.execute_script("arguments[0].click();", element)
-
-    #     element = browser.find_element_by_xpath("//*[@id=\"date-config\"]/div[1]/div/table/tbody/tr/td[1]/div/table/tbody/tr[" + str(year) + "]/td")
-    #     # element.click()
-    #     browser.execute_script("arguments[0].click();", element)
-
-    #     for month in range(1, 6):
-    #         if year == 1 and month == 1:
-    #             element = browser.find_element_by_xpath("//*[@id=\"date-config\"]/div[1]/div/table/tbody/tr/td[2]/div/table/tbody/tr[2]/td[4]")
-    #             # element.click()
-    #             browser.execute_script("arguments[0].click();", element)
-    #         elif year == 1:
-    #             element = browser.find_element_by_xpath("//*[@id=\"date-config\"]/div[1]/div/table/tbody/tr/td[2]/div/table/tbody/tr[3]/td[" + str(month-1) +"]")
-    #             # element.click()
-    #             browser.execute_script("arguments[0].click();", element)
-    #         elif year == 2 and month != 5:
-    #             element = browser.find_element_by_xpath("//*[@id=\"date-config\"]/div[1]/div/table/tbody/tr/td[2]/div/table/tbody/tr[1]/td[" + str(month) + "]")
-    #             # element.click()
-    #             browser.execute_script("arguments[0].click();", element)
-    #         else:
-    #             element = browser.find_element_by_xpath("//*[@id=\"date-config\"]/div[1]/div/table/tbody/tr/td[2]/div/table/tbody/tr[2]/td[1]")
-    #             # element.click()
-    #             browser.execute_script("arguments[0].click();", element)
-
-    #         sleep(3)
-    #         currMatches = browser.find_elements_by_class_name("result")
-
-    #         cur_win = browser.current_window_handle # get current/main window
-
-    #         for match in currMatches:
-
-    #             matchLink = match.find_elements_by_tag_name("a")
-    #             for link in matchLink:
-    #                 # link.click()
-    #                 link.send_keys(Keys.SHIFT + Keys.RETURN)
-    #                 sleep(10)
-    #             # Open the link in a new window by sending key strokes on the element
-    #             # match.send_keys(Keys.COMMAND + 't')
-
-    #             # Get windows list and put focus on new window (which is on the 1st index in the list)
-    #             # windows = browser.window_handles
-    #             # browser.switch_to.window(windows[1])
-
-    #             browser.switch_to_window([win for win in browser.window_handles if win !=cur_win][0]) # switch to new window
-
-    #             element = browser.find_element_by_xpath("//*[@id=\"live-match-options\"]/li[3]/a")
-    #             browser.execute_script("arguments[0].click();", element)
-    #             # time.sleep(5)
-
-    #             home = {};
-    #             away = {};
-
-    #             search =  browser.find_element_by_xpath("//*[@id=\"match-centre-header\"]/div[1]/div[2]/a") # home name
-    #             home['Name'] = search.text
-
-    #             search =  browser.find_element_by_xpath("//*[@id=\"match-centre-header\"]/div[1]/div[2]/div[3]") # home formation
-    #             home['Formation'] = search.text
-
-    #             search =  browser.find_element_by_xpath("//*[@id=\"match-centre-header\"]/div[3]/div[2]/a") # away name
-    #             away['Name'] = search.text
-
-    #             search =  browser.find_element_by_xpath("//*[@id=\"match-centre-header\"]/div[3]/div[2]/div[3]") # away formation
-    #             away['Formation'] = search.text
-
-    #             for i in range(1, 17):
-    #                 element = browser.find_element_by_xpath("//*[@id=\"event-type-filters\"]/li[" + str(i) + "]")
-    #                 browser.execute_script("arguments[0].click();", element)
-
-    #                 title = browser.find_element_by_xpath("//*[@id=\"event-type-filters\"]/li[" + str(i) + "]/a/h4") # title
-    #                 search = browser.find_element_by_xpath("//*[@id=\"event-type-filters\"]/li[" + str(i) + "]/a/div/span[1]") # home attribute
-    #                 home[title.text] = search.text
-
-    #                 search = browser.find_element_by_xpath("//*[@id=\"event-type-filters\"]/li[" + str(i) + "]/a/div/span[3]") # away attribute
-    #                 away[title.text] = search.text
-
-    #                 if i == 1:
-    #                     home["Goals"] = browser.find_element_by_xpath("//*[@id=\"chalkboard\"]/div[2]/div[1]/div[2]/div[2]/span[1]").text
-    #                     home["Shots on Target"] = browser.find_element_by_xpath("//*[@id=\"chalkboard\"]/div[2]/div[1]/div[2]/div[3]/span[1]").text
-
-    #                     away["Goals"] = browser.find_element_by_xpath("//*[@id=\"chalkboard\"]/div[2]/div[1]/div[2]/div[2]/span[2]").text
-    #                     away["Shots on Target"] = browser.find_element_by_xpath("//*[@id=\"chalkboard\"]/div[2]/div[1]/div[2]/div[3]/span[2]").text
-
-    #                     if int(home["Goals"]) > int(away["Goals"]):
-    #                         home["Result"] = 3
-    #                         away["Result"] = 0
-    #                     elif int(home["Goals"]) < int(away["Goals"]):
-    #                         home["Result"] = 0
-    #                         away["Result"] = 3
-    #                     else:
-    #                         home["Result"] = 1
-    #                         away["Result"] = 1
-    #         # //*[@id="chalkboard-timeline"]/div[1]/div[2]/div
-    #         # //*[@id="chalkboard-timeline"]/div[1]/div[3]/div[1]
-    #         # //*[@id="chalkboard-timeline"]/div[1]/div[3]/div[2]
-    #                 # search = browser.find_element_by_xpath("//*[@id=\"chalkboard-timeline\"]/div[3]/div[1]/div")
-    #                 # print(search.get_attribute('data-minute'))
-
-    #             print(home)
-    #             print(away)
-    #             with open('epl1718.csv', 'a', newline = '') as outfile:
-    #                 w = csv.writer(outfile)
-    #                 if numMatch == 0:
-    #                     w.writerow(home.keys())
-    #                 w.writerow(home.values())
-    #                 w.writerow(away.values())
-
-    #             numMatch = numMatch + 1
-    #             browser.close() # close new window
-    #             browser.switch_to_window(cur_win) # switch back to main window
+        browser.close() # close new window
+        browser.switch_to_window(opp_win) # switch back to main window
 
 except Exception as e:
     print(e)
